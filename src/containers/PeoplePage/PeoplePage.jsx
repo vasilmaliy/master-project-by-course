c80bf43bde90f66e1d9b1061505f0fedcc67eb11
Maliy Vasyl <vasil.maliy14@gmail.com>
2021-10-25 21:04:25 +0300

    lesson 6. Created higher order component for for error handling

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,27 +1,34 @@
 import { useState, useEffect } from 'react';
+import { withErrorApi } from '../../hoc-helpers/withErrorApi';
 import { getApiResource } from '../../utils/network';
 import { API_PEOPLE } from '../../constants/api';
 import { getPeopleId, getPeopleImage } from '../../services/getPeopleData';
 import PeopleList from '../../components/PeoplePage/PeopleList';
 
-const PeoplePage = () => {
+const PeoplePage = ({ setErrorApi }) => {
   const [people, setPeople] = useState(null);
 
-  const getResourse = async (url) => {
+  const getResourse = async ( url ) => {
     const res = await getApiResource(url);  
 
-    const peopleList = res.results.map(({ name, url }) => {
-      const id = getPeopleId(url);
-      const img = getPeopleImage(id);
+    if (res) {
+      const peopleList = res.results.map(({ name, url }) => {
+        const id = getPeopleId(url);
+        const img = getPeopleImage(id);
+  
+        return {
+          id,
+          name, 
+          img
+        }
+      })
+  
+      setPeople(peopleList); 
+      setErrorApi(false);
+    } else {
+      setErrorApi(true);
+    }
 
-      return {
-        id,
-        name, 
-        img
-      }
-    })
-
-    setPeople(peopleList);
   }
 
   useEffect(() => {
@@ -30,9 +37,10 @@ const PeoplePage = () => {
 
   return (
     <>
+      <h1>Navigation</h1>
       {people && <PeopleList people={people}/>}
     </>
   )
 }
 
-export default PeoplePage;
+export default withErrorApi(PeoplePage);
